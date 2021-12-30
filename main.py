0cfbb917029c3bf7b2518fadab1466367107ea91
Yacine <kandjiyacine@gmail.com>
2021-12-30 19:54:59 +0100

    Implémentation du contrôleur et de la vue pour notre application

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 """Entry point."""
 
-from models.card import Card
+from controllers.base import Controller
 from models.deck import Deck
-from models.player import Player
+from views.base import View
 
-if __name__ == "__main__":
-    card1 = Card("diamonds", "cinq")
-    card2 = Card("coeurs", "cinq")
-    card3 = Card("coeurs", "valet")
-    print(card1 < card2)
-    print(card3 > card2)
 
+def main():
     deck = Deck()
-    print(deck)
+    view = View()
+    game = Controller(deck, view)
+    game.run()
 
-    player = Player("James")
-    player.hand.append(deck.draw_card())
-    print(player.hand)
+if __name__ == '__main__':
+    main()
+
+
+# if __name__ == "__main__":
+#     card1 = Card("diamonds", "cinq")
+#     card2 = Card("coeurs", "cinq")
+#     card3 = Card("coeurs", "valet")
+#     print(card1 < card2)
+#     print(card3 > card2)
+#
+#     deck = Deck()
+#     print(deck)
+#
+#     player = Player("James")
+#     player.hand.append(deck.draw_card())
+#     print(player.hand)
